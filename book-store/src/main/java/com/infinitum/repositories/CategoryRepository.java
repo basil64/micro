4366202eb36d6e7e4eb9ece69f5b1a21039e0289
przi <piotr.ziembicki@gft.com>
2023-02-28 17:14:55 +0100

    Chnages in CategoryRepository

--- book-store/src/main/java/com/infinitum/repositories/CategoryRepository.java
+++ book-store/src/main/java/com/infinitum/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.infinitum.repositories;
 import com.infinitum.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Override
+    Optional<Category> findById(Long aLong);
 }
